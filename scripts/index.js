985512cbc753de68dfb42b1cfb7a670cf483edb5
Jamen Marz <jamen.marz@gmail.com>
2015-10-31 17:43:41 -0600

    Change layout

--- scripts/index.js
+++ scripts/index.js
@@ -12,25 +12,27 @@
         if (!repo.fork) {
           content.append(
             "<div class='repo'>" +
-              "<span class='name'>" +
-                "<a target='_blank' href='" + repo.html_url + "'>" + repo.full_name + "</a>" +
-              "</span>" +
+              "<div class='info'>" +
+                "<span class='name'>" +
+                  "<a target='_blank' href='" + repo.html_url + "'>" + repo.full_name + "</a>" +
+                "</span>" +
+                "<span class='desc'>" + repo.description + "</span>" +
+              "</div>" +
               "<div class='items'>" +
-                (repo.language ? "<span class='lang'>(" + repo.language + ")</span>" : '') +
                 "<span class='stars icon'>" +
                   "<a href='" + repo.html_url + "/stargazers'>" + repo.stargazers_count + "</a>" +
                 "</span>" +
                 "<span class='forks icon'>" +
                   "<a href='" + repo.html_url + "/forks'>" + repo.forks + "</a>" +
                 "</span>" +
+                (repo.language ? "<span class='lang'>(" + repo.language + ")</span>" : '') +
               "</div>" +
-              "<span class='desc'>" + repo.description + "</span>" +
             "</div>"
           );
         }
       });
     }).fail(function(){
-      $('.getting').addClass('failed').html('Could not fetch projects from GitHub.')
-    })
+      $('.getting').addClass('failed').html('Could not fetch projects from GitHub.');
+    });
   });
 })(jQuery);
