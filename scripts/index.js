1a390024b18c3e43c61aaeed88bcec4574bb13f1
Jack Palmer <jleeto7@gmail.com>
2016-01-31 01:22:27 -0600

    Fixed initial post error

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,20 @@
 (function() {
-  $(function() {
-    $.getJSON('https://api.github.com/users/jamen/repos?sort=pushed')
-    .success(function(repos) {
-      $('.project-repo').each(function(i) {
-        var project = repos[i];
-        console.log(project);
-        $(this).append(
-          '<div class="title">' +
-            '<span class="mega-octicon octicon-repo"></span>' +
-            '<span class="name">' + project.name + '</span>' +
-            '<span class="desc">' + project.description + '</span>' +
-          '</div>'
-        );
-      });
+    $(function() {
+        $.getJSON('https://api.github.com/users/jamen/repos?sort=pushed')
+            .success(function(repos) {
+                $('.project-repo').each(function(i) {
+                    var project = repos[i];
+                    console.log(project);
+                    $(this).append(
+                        '<div class="title">' +
+                        '<span class="mega-octicon octicon-repo"></span>' +
+                        '<span class="name">' + project.name + '</span>' +
+                        '<span class="desc">' + project.description + '</span>' +
+                        '</div>'
+                    );
+                });
+            });
+    });fail(function() {
+        $('.getting').addClass('failed').html('Error in connecting with Github.');
     });
-  }); .fail(function(){ $('.getting').addClass('failed').html('Error in connecting with Github.'); });
 })();
