c3f447a47292d3c84b75714c05192a9bb63a7843
Jamen Marz <jamen.marz@gmail.com>
2016-01-31 00:23:25 -0700

    Revert "added error message"

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,18 @@
 (function() {
-    $(function() {
-        $.getJSON('https://api.github.com/users/jamen/repos?sort=pushed')
-            .success(function(repos) {
-                $('.project-repo').each(function(i) {
-                    var project = repos[i];
-                    console.log(project);
-                    $(this).append(
-                        '<div class="title">' +
-                        '<span class="mega-octicon octicon-repo"></span>' +
-                        '<span class="name">' + project.name + '</span>' +
-                        '<span class="desc">' + project.description + '</span>' +
-                        '</div>'
-                    );
-                });
-            });
-    });fail(function() {
-        $('.getting').addClass('failed').html('Error in connecting with Github.');
+  $(function() {
+    $.getJSON('https://api.github.com/users/jamen/repos?sort=pushed')
+    .success(function(repos) {
+      $('.project-repo').each(function(i) {
+        var project = repos[i];
+        console.log(project);
+        $(this).append(
+          '<div class="title">' +
+            '<span class="mega-octicon octicon-repo"></span>' +
+            '<span class="name">' + project.name + '</span>' +
+            '<span class="desc">' + project.description + '</span>' +
+          '</div>'
+        );
+      });
     });
+  });
 })();
